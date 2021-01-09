25047fe5e5ed013331b41a47d05f2670431d2860
DorcasWanjiku <smallwanjiku@gmail.com>
2021-01-09 13:34:27 +0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './profile/about/about.component';
+import { ContactComponent } from './profile/contact/contact.component';
+import { EducationComponent } from './profile/education/education.component';
+import { ExperienceComponent } from './profile/experience/experience.component';
+import { FooterComponent } from './profile/footer/footer.component';
+import { HeaderComponent } from './profile/header/header.component';
+import { IntroComponent } from './profile/intro/intro.component';
+import { PortfolioComponent } from './profile/portfolio/portfolio.component';
+import { ProjectsComponent } from './profile/projects/projects.component';
+import { SkillsComponent } from './profile/skills/skills.component';
+import { ReferenceComponent } from './profile/reference/reference.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    EducationComponent,
+    ExperienceComponent,
+    FooterComponent,
+    HeaderComponent,
+    IntroComponent,
+    PortfolioComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ReferenceComponent
   ],
   imports: [
     BrowserModule,
